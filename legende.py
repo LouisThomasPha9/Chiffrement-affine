4061f1cbd9cd46b0ece08820716c309c7eba9022
Louis-Thomas Phaneuf <ltphaneuf9@gmail.com>
2025-12-10 20:45:51 -0500

    ajout de l'image et réorganisation de la légende

--- legende.py
+++
@@ -1,7 +0,0 @@
-import customtkinter as ctk
-import json
-
-class Legende(ctk.CTkFrame):
-    def __init__(self, main = None):
-        super().__init__(main)
-        # self.chars 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import json
 
 APP_GEOMETRY = "900x700"
 APP_TITLE = "Encription App"
@@ -9,4 +10,10 @@ class MainApp(ctk.CTk):
         ctk.set_appearance_mode("Dark") 
         self.geometry(APP_GEOMETRY)
         self.title(APP_TITLE)
-        
+        self.legende = json.load(open("characteres.json"))[0]
+        self.d = json.load(open("characteres.json"))[1]
+        print(self.d)
+
+if __name__ == "__main__":
+    app = MainApp() 
+    app.mainloop()
